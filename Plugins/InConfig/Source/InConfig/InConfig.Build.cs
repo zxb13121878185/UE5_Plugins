d7ed7d209154fc299eafb2c54c2017804c2892e2
ZXB <13121878185@163.com>
2024-11-10 12:59:30 +0800

    1

---
+++ Plugins/InConfig/Source/InConfig/InConfig.Build.cs
@@ -0,0 +1,61 @@
+// Some copyright should be here...
+
+using UnrealBuildTool;
+using System;
+using System.IO;
+
+public class InConfig : ModuleRules
+{
+	public InConfig(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+
+		string pluginPath = PluginDirectory;
+		string[] dirNameArray = pluginPath.Split("\\");
+		string projectName = dirNameArray[dirNameArray.Length - 3];
+
+		RuntimeDependencies.Add(Path.Combine(PluginDirectory, "../../Config", projectName + ".ini"));
+	}
+}
